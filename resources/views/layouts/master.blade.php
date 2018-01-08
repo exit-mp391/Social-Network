56431ae86f96391823fab923a364639db0d57fe4
Nenad Jordanov <nesssa91@hotmail.com>
2018-01-08 20:59:04 +0100

    Initial commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="{{ URL::to('src/css/main.css') }}">
+
+
+        <title>@yield('title')</title>
+       
+    </head>
+    <body>
+
+       @include('includes.header')
+
+       <div class="container">
+           @yield('content')
+           
+       </div>
+        
+
+        <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+        <script src="{{ URL::to('src/js/app.js') }}"></script>
+    </body>
+</html>
